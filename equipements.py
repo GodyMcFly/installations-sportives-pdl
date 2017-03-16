b51ad4264ba19ad477f946bada1166b2bff0b129
Lucas LEBANSAIS <E155981U@U-INF-J-C002-28.iut-nantes.univ-nantes.prive>
2017-03-16 11:57:24 +0100

    modification equipements.py pour l'insertion dans la BD

--- equipements.py
+++ equipements.py
@@ -1,25 +1,45 @@
 import csv
+from dao import *
 
-#
-# Ouverture du fichier source.
-#
-# D'après la documentation, le mode ''b'' est
-# *obligatoire* sur les plate-formes où il est
-# significatif. Dans la pratique, il est conseillé
-# de toujours le mettre.
-#
 
-fname = "csv/equipements.csv"
-fileEquipements = open(fname, "r")
 
-try:
-    # Création du ''lecteur'' CSV.
-    reader = csv.reader(fileEquipements)
+class equipements:
+    def __init__(self, numero, nom, numero_installation):
+        self.numero = numero
+        self.nom = nom
+        self.numero_installation = numero_installation
 
-    # Le ''lecteur'' est itérable, et peut être utilisé dans une boucle ''for'' pour extraire les lignes une par une.
-    for row in reader:
-	       print(row)
+    def _get_numero(self):
+		return self.numero
 
-finally:
-    # Fermeture du fichier source
-    fileEquipements.close()
+    def _get_nom(self):
+		return self.nom
+
+    def _get_numero_installation(self):
+        return self.numero_installation
+
+    def _set_numero(self, numero):
+		self.numero = numero
+
+    def _set_nom(self, nom):
+        self.nom = nom
+
+    def _set_numero_installation(self, numero_installation):
+        self.numero_installation
+
+
+    def Lecture(self, fichier):
+        with open(fichier) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                print(row)
+
+
+    def ajout(self):
+        ajoutEquipement = 'INSERT IGNORE INTO equipement VALUES ("{}","{}","{}")'.format(self.numero, self.nom, self.numero_installation)
+
+        db = DAO()
+        db.execute("LOCK TABLES `equipement` WRITE;")
+        db.execute("SET foreign_key_checks = 0")
+        db.execute(ajoutEquipement)
+        db.execute("SET foreign_key_checks = 1")
--- mainTestDAOetActivite.py
+++ mainTestDAOetActivite.py
@@ -1,10 +1,10 @@
 from dao import *
-from activites import *
+from equipements import *
 
 db = DAO()
 db._connect()
 #db.execute("INSERT into test(id) values(9)")
 #print(db.get_rows("SELECT * FROM `test`"))
-activite = Activite(1,"lol")
-activite.ajout()
-#activite.Lecture("csv/activites.csv")
+equipement = equipements(10,"La poutre en bois",1)
+equipement.ajout()
+#equipement.Lecture("csv/equipements.csv")
